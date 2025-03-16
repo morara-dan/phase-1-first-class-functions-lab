fd7ea7017a177c8d4daed66f56686c8daf40f97d
dancan <dancan.morara@student.moringaschool.com>
2025-03-16 12:53:59 +0300

    first-class

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = drivers => {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(0, 2);
+}    
+
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = drivers => {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return (drivers.slice(- 2));
+}
+
+returnLastTwoDrivers();
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) { 
+    return function(fare){ return fare * multiplier};
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drivers, choice){
+     return choice(drivers);
+}
+selectDifferentDrivers(returnFirstTwoDrivers());
